5250fa1ba8b707a15200eeb6457a7bff68cb9261
ju663rnaut <ju663rnaut@fastback.io>
2018-06-03 23:46:11 +0200

    updated codebuild ecr image build.sh script

--- codebuild/build.sh
+++ codebuild/build.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-docker build -t codebuild-go-test .
-docker tag codebuild-go-test:latest 692551067032.dkr.ecr.eu-central-1.amazonaws.com/codebuild-go-test:latest
-docker push 692551067032.dkr.ecr.eu-central-1.amazonaws.com/codebuild-go-test:latest
+if [ -z "$ECR_REPO_URI" ]; then
+    echo "environment variable 'ECR_REPO_URI' not set"
+    exit 1
+fi
 
+if ! [[ ${ECR_REPO_URI} =~ ^[0-9]{12}\.dkr.ecr.[a-z0-9\-]+.amazonaws.com/[a-z\-]+$ ]]; then
+    echo "environment variable 'ECR_REPO_URI' has invalid format"
+	exit 1
+fi
+
+docker build -t codebuild-go-test .
+docker tag codebuild-go-test:latest ${ECR_REPO_URI}:latest
+docker push ${ECR_REPO_URI}:latest
